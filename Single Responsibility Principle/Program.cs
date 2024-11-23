0547976bf1f936560baecfa102c332f8dda28b72
qiuxan <qiuxan@gmail.com>
2024-11-24 07:43:35 +1100

    Single_Responsibility_Principle Init app

---
+++ Single Responsibility Principle/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace Single_Responsibility_Principle;
+
+internal class Program
+{
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        var j = new Journal();
+        j.AddEntry("I cried today.");
+        j.AddEntry("I ate a bug.");
+        WriteLine(j);// a method from system.console
+
+    }
+}
