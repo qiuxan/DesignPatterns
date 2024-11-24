6ee69476a527c51bd796a9f0cf1a12888a9f7f29
qiuxan <qiuxan@gmail.com>
2024-11-24 13:18:33 +1100

    Open-Closed Principle Example, adding classes on top of interfaces

--- Open-Closed Principle/Program.cs
+++ Open-Closed Principle/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Open_Closed_Principle;
+﻿using static Open_Closed_Principle.Program;
+
+namespace Open_Closed_Principle;
 
 internal class Program
 {
@@ -56,6 +58,73 @@ internal class Program
         }
     }
 
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product p);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color _color;
+
+        public ColorSpecification(Color color)
+        {
+            _color = color;
+        }
+        bool ISpecification<Product>.IsSatisfied(Product p)
+        {
+            return p.Color == _color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size _size;
+
+        public SizeSpecification(Size size)
+        {
+            _size = size;
+        }
+        bool ISpecification<Product>.IsSatisfied(Product p)
+        {
+            return p.Size == _size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> _first, _second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            _first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            _second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return _first.IsSatisfied(p) && _second.IsSatisfied(p);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+      
+    }
+
     static void Main(string[] args)
     {
         var apple = new Product("Apple", Color.Green, Size.Small);
@@ -68,6 +137,28 @@ internal class Program
         Console.WriteLine("Green products (old):");
         foreach (var p in pf.FilterByColor(products, Color.Green))
             Console.WriteLine($" - {p.Name} is green");
+
+
+        var bf = new BetterFilter();
+        Console.WriteLine("Green products (new):");
+        foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            Console.WriteLine($" - {p.Name} is green");
+
+        Console.WriteLine("Large products");
+        foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+            Console.WriteLine($" - {p.Name} is large");
+
+        Console.WriteLine("Large blue items");
+        foreach (var p in bf.Filter
+            ( new AndSpecification<Product>
+                (
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large)
+                )))
+                   {
+                      Console.WriteLine($" - {p.Name} is big and blue");
+
+                   }
 
     }
 }
