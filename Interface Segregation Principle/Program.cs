e1ef048232362da11766ecb4fb747f68d37d0d7c
qiuxan <qiuxan@gmail.com>
2024-11-24 15:14:56 +1100

    Interface Segregation Principle, use small interface to combine a big one

--- Interface Segregation Principle/Program.cs
+++ Interface Segregation Principle/Program.cs
@@ -50,6 +50,65 @@ internal class Program
 
     // Segregated interfaces example:
 
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner // other interfaces are inherited
+    {
+
+    }
+
+    public class MultifunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter _printer;
+        private IScanner _scanner;
+
+        public MultifunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if(scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            _printer = printer;
+            _scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            _printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            _scanner.Scan(d);
+        }//decorator pattern
+    }
+
 
     static void Main(string[] args)
     {
