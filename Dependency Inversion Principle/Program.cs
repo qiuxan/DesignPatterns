80fd3f07332b52a3d28b9963c5e6c44c448f89a6
qiuxan <qiuxan@gmail.com>
2024-11-24 15:57:53 +1100

    Bad example of Dependency_Inversion_Principle: data calling function in Research class

---
+++ Dependency Inversion Principle/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Dependency_Inversion_Principle;
+
+internal class Program
+{
+
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+    //low-level module
+    public class Relationships 
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    public class Research
+    {
+        public Research(Relationships relationships)
+        {
+            var relations = relationships.Relations;
+            foreach (var r in relations.Where(
+                x => x.Item1.Name == "John" &&
+                     x.Item2 == Relationship.Parent
+            ))
+            {
+                Console.WriteLine($"John has a child called {r.Item3.Name}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Matt" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
+
+        }
+
+
+    }
+
+   
+}
