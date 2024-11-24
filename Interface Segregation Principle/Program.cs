88d0f3cab7975932f06061b0d8aedcf613f449a8
qiuxan <qiuxan@gmail.com>
2024-11-24 14:36:45 +1100

    Bad example of Interface_Segregation_Principle where oldfashionprinter can only print but have to take all the functions

---
+++ Interface Segregation Principle/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Interface_Segregation_Principle;
+
+internal class Program
+{
+    public class Document
+    {
+
+    }
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Segregated interfaces example:
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
