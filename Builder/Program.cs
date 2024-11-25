4cf1c2dfea5dd3c7f50851ea5c5dedbfbe1199a2
qiuxan <qiuxan@gmail.com>
2024-11-25 20:17:27 +1100

    Add project files.

---
+++ Builder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Builder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var hello = "Hello";
+
+        var sb = new StringBuilder();
+        sb.Append("<p>");
+        sb.Append(hello);
+        sb.Append("</p>");
+
+        Console.WriteLine(sb);
+
+        var words = new[] { "Hello", "world" };
+        Console.WriteLine(words);
+        // <ul><li>hello</li><li>world</li></ul>
+        sb.Clear();
+        sb.Append("<ul>");
+        foreach (var word in words)
+        {
+            sb.AppendFormat("<li>{0}</li>", word);
+
+        }
+        sb.Append("</ul>");
+
+        Console.WriteLine(sb);
+
+    }
+}
