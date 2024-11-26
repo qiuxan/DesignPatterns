7fb1b30e0bd8b1ecafbf62e6fd8b917ce799dd30
qiuxan <qiuxan@gmail.com>
2024-11-26 21:37:05 +1100

    Bad example of  Fluent Builder Inheritance

---
+++ Builder/Fluent Builder Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Fluent_Builder_Inheritance;
+
+public class Person
+{
+    public string Name;
+    public string Position;
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+    }
+}
+
+public class PersonBuilder
+{
+    protected Person person = new();
+
+    public PersonBuilder called(string name)
+    {
+        person.Name = name;
+        return this;
+    }
+}
+
+public class PersonJobBuilder : PersonBuilder
+{
+    public PersonJobBuilder worksAsA(string position)
+    {
+        person.Position = position;
+        return this;
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new PersonJobBuilder();
+        builder.called("Dmitri")
+            .worksAsA("Quant") // not work because called returns PersonBuilder not PersonJobBuilder
+            ;
+    }
+}
