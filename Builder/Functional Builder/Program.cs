964c01816af9ac474ca7be7b17948e4bfa1cf655
qiuxan <qiuxan@gmail.com>
2024-11-27 09:24:04 +1100

    A better practice of Functional Builder

--- Builder/Functional Builder/Program.cs
+++ Builder/Functional Builder/Program.cs
@@ -5,28 +5,56 @@ public class Person
     public string Name, Postion;
 }
 
-public sealed class PersonBuilder
+public abstract class FunctionalBuilder<TSubject, TSelf>
+    where TSelf : FunctionalBuilder<TSubject, TSelf>
+    where TSubject : new()
 {
     private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
 
-    public PersonBuilder Do(Action<Person> action) => AddAction(action);
-
-    public PersonBuilder Called(string name) => AddAction(p => { p.Name = name;});
+    public TSelf Do(Action<Person> action) => AddAction(action);
 
     public Person Build()
     => actions.Aggregate(new Person(), (p, f) => f(p));
 
-    private PersonBuilder AddAction(Action<Person> action)
+    private TSelf AddAction(Action<Person> action)
     {
         actions.Add(p =>
         {
             action(p);
             return p;
         });
-        return this;
+        return (TSelf)this;
     }
 
 }
+
+public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+{
+    public PersonBuilder Called(string name) => Do(p => { p.Name = name; });
+}
+
+//public sealed class PersonBuilder
+    //{
+    //    private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+
+    //    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+
+    //    public PersonBuilder Called(string name) => AddAction(p => { p.Name = name;});
+
+    //    public Person Build()
+    //    => actions.Aggregate(new Person(), (p, f) => f(p));
+
+    //    private PersonBuilder AddAction(Action<Person> action)
+    //    {
+    //        actions.Add(p =>
+    //        {
+    //            action(p);
+    //            return p;
+    //        });
+    //        return this;
+    //    }
+
+    //}
 
 public static class PersonBuilderExtensions
 {
