8717b77d71067ea63cbe16cc3f2af5b3fbcc1e36
qiuxan <qiuxan@gmail.com>
2024-11-27 09:45:40 +1100

    Finished Faceted Builder

--- Builder/Faceted Builder/Program.cs
+++ Builder/Faceted Builder/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Faceted_Builder
+﻿namespace Faceted_Builder;
 public class Person
 {
     // Address
@@ -18,6 +18,14 @@ public class PersonBuilder // facade
 {
     //reference
     protected Person person = new Person();
+
+    public PersonJobBuilder Works => new PersonJobBuilder(person);
+    public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+    public static implicit operator Person(PersonBuilder pb)
+    {
+        return pb.person;
+    }
 
 }
 
@@ -48,11 +56,48 @@ public class PersonJobBuilder : PersonBuilder
     }
 }
 
+public class PersonAddressBuilder : PersonBuilder
+{
+    public PersonAddressBuilder(Person person)
+    {
+        this.person = person;
+    }
+
+    public PersonAddressBuilder At(string streetAddress)
+    {
+        person.StreetAddress = streetAddress;
+        return this;
+    }
+
+    public PersonAddressBuilder WithPostcode(string postcode)
+    {
+        person.Postcode = postcode;
+        return this;
+    }
+
+    public PersonAddressBuilder In(string city)
+    {
+        person.City = city;
+        return this;
+    }
+}
+
 internal class Program
 {
     static void Main(string[] args)
     {
         var pb = new PersonBuilder();
+        Person person = 
+            pb
+            .Lives
+            .At("123 London Road")
+            .In("London")
+            .WithPostcode("SW12BC")
+            .Works
+            .At("Fabrikam")
+            .AsA("Engineer")
+            .Earning(123000);
+        Console.WriteLine(person);
 
     }
 }
