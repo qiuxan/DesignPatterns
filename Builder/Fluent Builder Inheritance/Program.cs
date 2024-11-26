307267a2764e5e95909aa13ba4122f3965f4e644
qiuxan <qiuxan@gmail.com>
2024-11-27 07:39:06 +1100

    Fix the issue with Recursive Generics

--- Builder/Fluent Builder Inheritance/Program.cs
+++ Builder/Fluent Builder Inheritance/Program.cs
@@ -5,29 +5,48 @@ public class Person
     public string Name;
     public string Position;
 
+    public class Builder : PersonJobBuilder<Builder>
+    {
+        
+    }
+
+    public static Builder New => new Builder();
+
     public override string ToString()
     {
         return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
     }
 }
-
-public class PersonInfoBuilder
+public abstract class PersonBuilder
 {
     protected Person person = new();
 
-    public PersonInfoBuilder called(string name)
+    public Person Build()
     {
-        person.Name = name;
-        return this;
+        return person;
     }
 }
 
-public class PersonJobBuilder : PersonInfoBuilder
+public class PersonInfoBuilder<SELF> 
+    :PersonBuilder
+    where SELF : PersonInfoBuilder<SELF>
 {
-    public PersonJobBuilder worksAsA(string position)
+
+    public SELF Called(string name)
+    {
+        person.Name = name;
+        return (SELF)this;
+    }
+}
+
+public class PersonJobBuilder <SELF>
+    : PersonInfoBuilder<PersonJobBuilder<SELF>>
+    where SELF : PersonJobBuilder<SELF>
+{
+    public SELF worksAsA(string position)
     {
         person.Position = position;
-        return this;
+        return (SELF)this;
     }
 }
 
@@ -35,9 +54,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var builder = new PersonJobBuilder();
-        builder.called("Dmitri")
-            .worksAsA("Quant") // not work because called returns PersonBuilder not PersonJobBuilder
-            ;
+        var me = Person.New
+            .Called("ian")
+            .worksAsA("Developer")
+            .Build();
+        Console.WriteLine(me);
+
     }
 }
