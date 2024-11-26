d61b13a8560a7dc130ddce0be8a9d5c4ba8e3065
qiuxan <qiuxan@gmail.com>
2024-11-27 08:07:34 +1100

    Stepwise_Builder Init

---
+++ Builder/Stepwise Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Stepwise_Builder;
+
+public enum CarType
+{
+    Sedan,
+    Coupe,
+    Hatchback
+}
+public class Car
+{
+    public CarType Type;
+    public int WheelSize;
+    //task: when building a car, we want to set the type and then according to the type, set the wheel size
+    //3 steps using interfaces segregation principle
+    //1. define the interface for the car
+    //2. define the interface for setting the wheel size
+    //3. define the car builder
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
