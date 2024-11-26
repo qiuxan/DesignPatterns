f20b17c89f1210953aaab0b9f4490bedca31160b
qiuxan <qiuxan@gmail.com>
2024-11-27 09:12:46 +1100

    Basic Idea of Functional Builder

---
+++ Builder/Functional Builder/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Functional_Builder;
+
+public class Person
+{
+    public string Name, Postion;
+}
+
+public sealed class PersonBuilder
+{
+    private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+
+    public PersonBuilder Do(Action<Person> action) => AddAction(action);
+
+    public PersonBuilder Called(string name) => AddAction(p => { p.Name = name;});
+
+    public Person Build()
+    => actions.Aggregate(new Person(), (p, f) => f(p));
+
+    private PersonBuilder AddAction(Action<Person> action)
+    {
+        actions.Add(p =>
+        {
+            action(p);
+            return p;
+        });
+        return this;
+    }
+
+}
+
+public static class PersonBuilderExtensions
+{
+    public static PersonBuilder WorksAsA(this PersonBuilder builder, string position)
+    => builder.Do(p => p.Postion = position);
+}
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new PersonBuilder()
+            .Called("Ian")
+            .WorksAsA("Developer") // this is a fluent builder
+            .Build();
+    }
+}
