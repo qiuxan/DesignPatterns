7aca160e8aab3a636593e2193ac5535ecc74a477
qiuxan <qiuxan@gmail.com>
2024-11-28 09:17:22 +1100

    Trick to make the Inner Factory inside of a class

--- Factory/Point Example/Program.cs
+++ Factory/Point Example/Program.cs
@@ -1,39 +1,39 @@
 ﻿namespace Point_Example;
 
-public enum CoordinateSystem
-{
-    Cartesian,
-    Polar
-}
 
-public static class PointFactory
-{
-    public static Point NewCartesianPoint(double x, double y)
-    {
-        return new Point(x, y);
-    }
 
-    public static Point NewPolarPoint(double rho, double theta)
-    {
-        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-    }
-}
 
 public class Point
 {
     private double x, y;
 
-    public Point(double x, double y)
+    private Point(double x, double y)
     {
         this.x = x;
         this.y = y;
     }
 
+    public static Point Origin => new Point(0, 0);
+
+    public static Point Origin2 = new Point(0, 0);//better than above
     //factory method
 
     public override string ToString()
     {
         return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+    }
+
+    public static class Factory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
     }
 }
 
@@ -41,10 +41,15 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var p = new Point(2, 3);// now we can create a point like this
 
-        var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+        var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
+
+        var origin = Point.Origin;
+        Console.WriteLine(origin);
+
+        var origin2 = Point.Origin2;
+        Console.WriteLine(origin2);
 
     }
 }
