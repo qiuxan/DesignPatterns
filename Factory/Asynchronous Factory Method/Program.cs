cb27b117ec7b79887504eb51ac5745834191b9a5
qiuxan <qiuxan@gmail.com>
2024-11-28 07:33:22 +1100

    Async when creating an obj solution but not perfect

---
+++ Factory/Asynchronous Factory Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Asynchronous_Factory_Method;
+
+public class Foo
+{
+    public Foo()
+    {
+        //await Task.Delay(1000); // can't use await here!
+    }
+
+    public async Task<Foo> InitAsync()
+    {
+        await Task.Delay(1000);
+        return this;
+    }
+}
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var foo = new Foo();
+
+        await foo.InitAsync();
+
+    }
+}
