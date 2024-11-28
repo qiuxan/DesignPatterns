568b34552ad0a56fae8440b3a0af52f2a8195b31
qiuxan <qiuxan@gmail.com>
2024-11-29 06:41:25 +1100

    without Abstract factory idea

---
+++ Factory/Abstract Factory/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Abstract_Factory;
+
+public interface IHotDrink
+{
+    void Consume();
+}
+
+internal class Tea : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This tea is nice but I'd prefer it with milk.");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+    public void Consume()
+    {
+        Console.WriteLine("This coffee is sensational!");
+    }
+}
+//every time we add a new drink, we need to add a new class
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
