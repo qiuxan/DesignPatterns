3c4494ef092343be53c186763f6ec0af141f3220
qiuxan <qiuxan@gmail.com>
2024-12-04 22:14:09 +1100

    Singleton in Dependency Injection

--- Singleton/Singleton Implementation/Program.cs
+++ Singleton/Singleton Implementation/Program.cs
@@ -48,6 +48,62 @@ public class SingletonDatabase : IDatabase
     public static SingletonDatabase Instance => instance.Value;
 }
 
+public class OrdinaryDatabase: IDatabase
+{
+    private Dictionary<string, int> capitals;
+
+    public OrdinaryDatabase()
+    {
+        WriteLine("Initializing database");
+
+        capitals = File.ReadAllLines("./capital.txt")
+            .Batch(2)
+            .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1))
+            );
+    }
+    public int GetPopulation(string name)
+    {
+        return capitals[name];
+    }
+
+}
+
+public class ConfigurableRecordFinder
+{
+    private IDatabase database;
+
+    public ConfigurableRecordFinder(IDatabase database)
+    {
+        this.database = database;
+    }
+
+    public int GetTotalPopulation(IEnumerable<string> names)
+    {
+        int result = 0;
+        foreach (var name in names)
+        {
+            result += database.GetPopulation(name);
+        }
+
+        return result;
+    }
+}
+
+public class DummyDatabase : IDatabase
+{
+    public int GetPopulation(string name)
+    {
+        return new Dictionary<string, int>
+        {
+            ["alpha"] = 1,
+            ["beta"] = 2,
+            ["gamma"] = 3
+        }[name];
+    }
+}
+
 [TestFixture]
 public class SingletonTests
 {
@@ -66,6 +122,27 @@ public class SingletonTests
         var names = new[] { "Seoul", "Mexico City" };
         int tp = rf.GetTotalPopulation(names);
         Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+    }
+
+    [Test]
+    public void ConfigurablePopulationTest()
+    {
+        var rf = new ConfigurableRecordFinder(new DummyDatabase());
+        var names = new[] { "alpha", "gamma" };
+        int tp = rf.GetTotalPopulation(names);
+        Assert.That(tp, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void DIPopulationTest()
+    {
+        var cb = new ContainerBuilder();
+        cb.RegisterType<OrdinaryDatabase>().As<IDatabase>().SingleInstance();
+        cb.RegisterType<ConfigurableRecordFinder>();
+        using (var c = cb.Build())
+        {
+            var rf = c.Resolve<ConfigurableRecordFinder>();
+        }
     }
 }
 
