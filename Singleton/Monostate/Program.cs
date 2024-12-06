196450b051faea1c48141bc9747fe4981b6e39cf
qiuxan <qiuxan@gmail.com>
2024-12-06 15:38:37 +1100

    Monostate

---
+++ Singleton/Monostate/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Monostate;
+
+class CEO
+{
+    private static string name;
+    private static int age;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+    }
+}
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        var ceo1 = new CEO();
+        ceo1.Name = "Adam";
+        ceo1.Age = 55;
+
+        var ceo2 = new CEO();
+        Console.WriteLine(ceo2);
+
+    }
+
+}
