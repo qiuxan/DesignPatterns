e1326b53cb9d80d6369add1d61faae348d12c4e2
qiuxan <qiuxan@gmail.com>
2024-12-06 16:17:32 +1100

    Per-Thread Singleton

---
+++ Singleton/Per-Thread Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Per_Thread_Singleton;
+
+public sealed class PerThreadSingleton
+{
+    private static ThreadLocal<PerThreadSingleton> threadInstance = new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+    public int id;
+
+    private PerThreadSingleton()
+    {
+        id = Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public static PerThreadSingleton Instance => threadInstance.Value;
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var t1 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("t1:"+PerThreadSingleton.Instance.id);
+        });
+
+        var t2 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("t2:" + PerThreadSingleton.Instance.id);
+            Console.WriteLine("t2:" + PerThreadSingleton.Instance.id);
+
+        });
+
+        Task.WaitAll(t1, t2);
+    }
+}
