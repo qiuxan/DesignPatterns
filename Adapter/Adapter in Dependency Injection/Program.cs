4569784a7ce66999bd22d2b23afe8e540fe30e0d
qiuxan <qiuxan@gmail.com>
2024-12-11 12:44:39 +1100

    Adapter in Dependency Injection

--- Adapter/Adapter in Dependency Injection/Program.cs
+++ Adapter/Adapter in Dependency Injection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Autofac.Features.Metadata;
 
 namespace Adapter_in_Dependency_Injection;
 
@@ -28,20 +29,28 @@ public class Button
     private ICommad _command;
     private string _name;
 
-    public Button(ICommad command)
+    public Button(ICommad command, string name)
     {
         _command = command;
+        _name = name;
     }
 
     public void Click()
     {
         _command.Execute();
     }
+
+    public void PrintMe()
+    {
+        Console.WriteLine($"I am a button called {_name}");
+    }
 }
 
 public class Editor
 {
     private IEnumerable<Button> buttons;
+
+    public IEnumerable<Button> Buttons => buttons;
 
     public Editor(IEnumerable<Button> buttons)
     {
@@ -64,16 +73,24 @@ internal class Program
     static void Main(string[] args)
     {
         var b = new ContainerBuilder();
-        b.RegisterType<SaveCommand>().As<ICommad>();
-        b.RegisterType<OpenCommand>().As<ICommad>();
+        b.RegisterType<SaveCommand>().As<ICommad>()
+            .WithMetadata("Name","Save");
+        b.RegisterType<OpenCommand>().As<ICommad>()
+            .WithMetadata("Name", "Open");
         //b.RegisterType<Button>();
-        b.RegisterAdapter<ICommad, Button>(cmd => new Button(cmd));
+        //b.RegisterAdapter<ICommad, Button>(cmd => new Button(cmd));
+
+        b.RegisterAdapter<Meta<ICommad>,Button>(cmd => new Button(cmd.Value, (string)cmd.Metadata["Name"]));
         b.RegisterType<Editor>();
 
         using (var c = b.Build())
         {
             var editor = c.Resolve<Editor>();
-            editor.ClickAll();
+            //editor.ClickAll();
+            foreach (var button in editor.Buttons)
+            {
+                button.PrintMe();
+            }
         }
     }
 }
