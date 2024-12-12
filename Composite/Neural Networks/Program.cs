5d8b69ca1de21ebf614b0c96f004e8eb49e3fb41
qiuxan <qiuxan@gmail.com>
2024-12-12 15:07:16 +1100

    Ini the neuron class

---
+++ Composite/Neural Networks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Neural_Networks;
+
+public class Neuron
+{
+    public float Value;
+    public List<Neuron> In, Out;
+
+    public void ConnectTo(Neuron other)
+    {
+        Out.Add(other);
+        other.In.Add(this);
+    }
+
+    
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var neuron1 = new Neuron();
+        var neuron2 = new Neuron();
+
+        neuron1.ConnectTo(neuron2);
+    }
+}
