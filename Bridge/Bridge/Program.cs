404cdc8fe7963e69ffd6694c97ef547b108ddbe3
qiuxan <qiuxan@gmail.com>
2024-12-12 11:53:17 +1100

    A DI solution of Bridge

--- Bridge/Bridge/Program.cs
+++ Bridge/Bridge/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Bridge;
+﻿using Autofac;
+
+namespace Bridge;
 
 
 public interface IRenderer
@@ -59,12 +61,23 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        //IRenderer renderer = new RasterRenderer();
-        IRenderer renderer = new VectorRenderer();
-        var circle = new Circle(renderer, 5);
+        var cb = new ContainerBuilder();
 
-        circle.Draw();
-        circle.Resize(2);
-        circle.Draw();
+        cb.RegisterType<VectorRenderer>().As<IRenderer>()
+            .SingleInstance();// make a sigleton
+
+        cb.Register((c, p) =>
+            new Circle(c.Resolve<IRenderer>(),
+                p.Positional<float>(0)));
+
+        using (var c = cb.Build())
+        {
+            var circle = c.Resolve<Circle>(
+                new PositionalParameter(0, 5.0f)
+            );
+            circle.Draw();
+            circle.Resize(2.0f);
+            circle.Draw();
+        }
     }
 }
