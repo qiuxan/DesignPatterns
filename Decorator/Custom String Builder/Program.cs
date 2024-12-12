2cf892bc9e43a91ff7cf7af5a63dd9e4faadf674
qiuxan <qiuxan@gmail.com>
2024-12-12 16:08:33 +1100

    Ini to extend the StringBuilder --with Problem

---
+++ Decorator/Custom String Builder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Custom_String_Builder;
+
+public class CodeBuilder : StringBuilder // cannot do this because StingBuildern is sealed!
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
