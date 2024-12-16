07440d53e104ff9519a8a5f4676ce5a277980ed2
qiuxan <qiuxan@gmail.com>
2024-12-16 14:36:17 +1100

    Example of cannot use multiple inheritance in C#

---
+++ Decorator/Multiple Inheritance with Interfaces/Multiple Inheritance with Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Bird
+{
+    public void Fly()
+    {
+        Console.WriteLine("Soaring in the sky");
+    }
+}
+
+public class Lizard
+{
+    public void Crawl()
+    {
+        Console.WriteLine("Scooting on the ground");
+    }
+}
+
+
+public class Dragon: Bird, Lizard// cannot use multiple inheritance in C#
+{
+
+}
+
+
+
+
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+     
+    }
+}
